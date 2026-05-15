32e47d2ef54b814d8b33c0da2b9d82be20713b45
Carlos Bezerra <crabezerra@gmail.com>
2026-05-15 00:17:40 -0300

    atualiza catalogo

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 from flask import Flask, render_template
-import csv
+import mysql.connector
 
 app = Flask(__name__)
 
+def db():
+    return mysql.connector.connect(
+        host="SERVIDOR",
+        user="root",
+        password="123456",
+        database="sysloja"
+    )
+
 @app.route("/")
 def home():
+    conn = db()
+    cur = conn.cursor(dictionary=True)
+
+    cur.execute("""
+        SELECT 
+            p.CodPro,
+            p.DesPro,
+            p.PcoVen,
+            c.ordem
+        FROM catalogo c
+        JOIN produtos p ON p.CodPro = c.produto_id
+        WHERE c.ativo = 1
+        ORDER BY c.ordem
+    """)
+
+    dados = cur.fetchall()
 
     produtos = []
 
-    with open("catalogo.csv", newline="", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-
-        for row in reader:
-            if row["ativo"] == "1":
-
-                produtos.append({
-                    "nome": f"Produto {row['produto_id']}",
-                    "preco": "R$ 0,00",
-                    "descricao": "",
-                    "imagem": f"/static/imagens/{row['produto_id']}.jpg"
-                })
+    for p in dados:
+        produtos.append({
+            "nome": p["DesPro"],
+            "preco": f"R$ {p['PcoVen']:.2f}" if p["PcoVen"] else "R$ 0,00",
+            "descricao": "",
+            "imagem": f"/static/imagens/{p['CodPro']}.jpg"
+        })
 
     return render_template("index.html", produtos=produtos)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
