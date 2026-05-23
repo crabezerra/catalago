ddfa38ade590d496e742163cd0e9b8f91233b665
Carlos Bezerra <crabezerra@gmail.com>
2026-05-23 16:50:01 -0300

    corrige conexao mariadb ipv6

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 db = pymysql.connect(
     host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT", 3306)),
+    port=3306
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME"),
